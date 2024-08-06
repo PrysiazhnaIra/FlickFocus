ae1cf3954e746c423b51ddcd5b9a122253adcaf8
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-08-06 22:52:24 +0300

    LoadMore for MoviePage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -8,10 +8,15 @@ import {
 import { searchMovies } from "../../api/tmdbApi";
 import MovieList from "../../components/MovieList/MovieList";
 import SearchForm from "../../components/SearchForm/SearchForm";
-// import css from "./MoviesPage.module.css";
+import Loader from "../../components/Loader/Loader";
+import css from "./MoviesPage.module.css";
 
 export default function MoviesPage() {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [initialLoad, setInitialLoad] = useState(false);
 
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get("query") ?? "";
@@ -22,24 +27,51 @@ export default function MoviesPage() {
     if (query) {
       const fetchMovies = async () => {
         try {
-          const searchResult = await searchMovies(query);
-          setMovies(searchResult.results);
+          setLoading(true);
+          const searchResult = await searchMovies(query, page);
+
+          setMovies((prevMovies) => {
+            if (page === 1) {
+              return searchResult.results;
+            } else {
+              return [...prevMovies, ...searchResult.results];
+            }
+          });
+          if (searchResult.results.length === 0) {
+            setHasMore(false);
+          }
+          if (page === 1) {
+            setInitialLoad(true);
+          }
         } catch (error) {
           console.error("Error searching movies", error);
+        } finally {
+          setLoading(false);
         }
       };
       fetchMovies();
     }
-  }, [query]);
+  }, [query, page]);
 
   const handleSearch = (value) => {
     setSearchParams({ query: value });
   };
 
+  const handleLoadMore = () => {
+    if (hasMore && !loading) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
   return (
     <div>
       <SearchForm onSearch={handleSearch} initialQuery={query} />
       <MovieList movies={movies} />
+      {loading && <Loader />}
+      {initialLoad && hasMore && !loading && (
+        <button onClick={handleLoadMore} className={css.loadMoreButton}>
+          Load more
+        </button>
+      )}
     </div>
   );
 }
